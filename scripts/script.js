06bfe5c5229ecae8608d07f8f6b48c5313990564
Tatiana <ganzhinovat@yandex.ru>
2023-01-19 20:33:55 +0300

    feat: implement Slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,4 @@
 // ============== SLIDER ============== 
-
-//const btnLeft = document.querySelector('.slider__arrow_left');
-//const btnRight = document.querySelector('.slider__arrow_right');
-
 const addPreloader = (elem) => {
   elem.classList.add('preload');
 };
@@ -11,8 +7,55 @@ const removePreloader = (elem) => {
   elem.classList.remove('preload');
 };
 
-const launchSlider = (elem) => {
-  removePreloader(elem);
+const launchSlider = () => {
+  const sliderItems = document.querySelectorAll('.slider__item');
+  const sliderList = document.querySelector('.slider__list');
+  const btnPrev = document.querySelector('.slider__arrow_left');
+  const btnNext = document.querySelector('.slider__arrow_right');
+
+  let activeSlide = 1;
+  let position = 0;
+
+  const checkSlide = () => {
+    if (activeSlide + 2 === sliderItems.length) {
+      btnNext.style.display = 'none';
+    } else {
+      btnNext.style.display = '';
+    }
+
+    if (activeSlide === 1) {
+      btnPrev.style.display = 'none';
+    } else {
+      btnPrev.style.display = '';
+    }
+  };
+
+  checkSlide();
+
+  const nextSlide = () => {
+    sliderItems[activeSlide].classList.remove('slider__item_active');
+    position = -sliderItems[0].clientWidth * activeSlide;
+
+    sliderList.style.transform = `translateX(${position}px)`;
+    activeSlide += 1;
+    sliderItems[activeSlide].classList.add('slider__item_active');
+
+    checkSlide();
+  };
+
+  const prevSlide = () => {
+    sliderItems[activeSlide].classList.remove('slider__item_active');
+    position = -sliderItems[0].clientWidth * (activeSlide - 2);
+
+    sliderList.style.transform = `translateX(${position}px)`;
+    activeSlide -=1;
+    sliderItems[activeSlide].classList.add('slider__item_active');
+
+    checkSlide();
+  };
+
+  btnPrev.addEventListener('click', prevSlide);
+  btnNext.addEventListener('click', nextSlide);
 };
 
 const initSlider = () => {
@@ -22,11 +65,11 @@ const initSlider = () => {
   sliderContainer.style.display = 'none';
 
   addPreloader(slider);
-  console.log('загрузка прелоадера...');
 
   window.addEventListener('load', () => {
-    launchSlider(slider);
     sliderContainer.style.display = '';
+    removePreloader(slider);
+    launchSlider();
   });
 };
 
