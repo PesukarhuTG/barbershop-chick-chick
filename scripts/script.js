8e6d2233b58ddc34ff84128c33f8334d675f96bc
Tatiana <ganzhinovat@yandex.ru>
2023-01-21 22:36:59 +0300

    feat: implement getting service data from API

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const API_URL = 'https://tame-adjoining-paprika.glitch.me/api';
+
 // ============== SLIDER ============== 
 const addPreloader = (elem) => {
   elem.classList.add('preload');
@@ -91,6 +93,68 @@ const initSlider = () => {
   });
 };
 
-window.addEventListener('DOMContentLoaded', initSlider);
+// отрисовка услуг со стоимостью (вариант через forEach)
+const renderPrice = (elem, data) => {
+  data.forEach(item => {
+    const priceItem = document.createElement('li');
+    priceItem.classList.add('price__item');
+    
+    priceItem.innerHTML = `
+        <span class="price__item-title">${item.name}</span>
+        <span class="price__item-sum">${item.price} руб</span>
+    `;
+
+    elem.append(priceItem);
+  });
+};
+
+// отрисовка услуг в форме (вариант через map и отрисовку массива)
+const renderService = (elem, data) => {
+  const labels = data.map(item => {
+    const label = document.createElement('label');
+    label.classList.add('radio');
+
+    label.innerHTML = `
+        <input class="radio-input" type="radio" name="service" value="${item.id}">
+        <span class="radio-label">${item.name}</span>
+    `;
+
+    return label;
+  });
+
+  elem.append(...labels);
+};
+
+// получение списка услуг с сервера
+const initService = () => {
+  const priceList = document.querySelector('.price__list');
+  const reserveFieldsetService = document.querySelector('.reserve__fieldset_service');
+  
+  priceList.textContent = '';
+  addPreloader(priceList);
+
+  reserveFieldsetService.innerHTML = '<legend class="reserve__legend">Услуга</legend>';
+  addPreloader(reserveFieldsetService);
+
+  fetch(API_URL).then(res => res.json())
+                .then(data => {
+                  renderPrice(priceList, data);
+                  removePreloader(priceList);
+                  return data;
+                })
+                .then(data => {
+                  renderService(reserveFieldsetService, data);
+                  removePreloader(reserveFieldsetService);
+                })
+                .catch(err => err);
+};
+
+const init = () => {
+  initSlider();
+  initService();
+
+};
+
+window.addEventListener('DOMContentLoaded', init);
 
 
